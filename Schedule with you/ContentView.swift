fadb0241f005b44e6464a6c6863ed257aa77afd5
ChenCheng <cchengoran@gmail.com>
2026-06-13 23:30:30 +0800

    Initial Commit

---
+++ Schedule with you/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Schedule with you
+//
+//  Created by 陈铭远 on 2026/6/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Schedule with you/Schedule_with_youApp.swift
@@ -0,0 +1,17 @@
+//
+//  Schedule_with_youApp.swift
+//  Schedule with you
+//
+//  Created by 陈铭远 on 2026/6/13.
+//
+
+import SwiftUI
+
+@main
+struct Schedule_with_youApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
